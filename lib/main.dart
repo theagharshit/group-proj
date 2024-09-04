d389b1aa23ebaffed6418b076f2bbbec2e1588f1
samir0607 <samir.gupta987@gmail.com>
2024-09-04 21:18:35 +0530

    Login Page Interactive 14

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen());
+      home: LoginScreen());
   }
 }
